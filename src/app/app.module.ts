2d4c656ba44c2756791828eaf7625bd3c4da8266
crisgon123 <crisgon123@gmail.com>
2019-09-23 22:22:57 -0500

    Subiendo final de la clase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ProductoComponent } from './components/producto/producto.components';
+import { ProductoComponent } from './components/producto/producto.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductoComponent
+    ProductoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
